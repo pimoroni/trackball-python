f2a34f76232073011bf407c871d476959432bf3c
Phil Howard <phil@gadgetoid.com>
2019-04-17 12:05:28 +0100

    Mock smbus2

--- library/tests/test_setup.py
+++ library/tests/test_setup.py
@@ -5,7 +5,7 @@ import mock
 def test_setup():
     sys.modules['RPi'] = mock.Mock()
     sys.modules['RPi.GPIO'] = mock.Mock()
-    sys.modules['smbus'] = mock.Mock()
+    sys.modules['smbus2'] = mock.Mock()
     import trackball
     device = trackball.TrackBall()
     del device
