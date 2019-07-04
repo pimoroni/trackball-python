a3a4fc6727f7557fcdb3558d4f9a33d4bd4cd638
Phil Howard <phil@gadgetoid.com>
2019-07-04 11:13:37 +0100

    Fix mouse.py example

--- examples/mouse.py
+++ examples/mouse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import time
 import os
+import math
 from trackball import TrackBall
 
 print("""Trackball: Mouse
@@ -17,22 +18,22 @@ trackball.set_rgbw(0, 0, 0, 0)
 # Check for xte (used to control mouse)
 use_xte = os.system('which xte') == 0
 
+if use_xte == 0:
+    raise RuntimeError("xte not found. Did you sudo apt install xautomation?")
+
 while True:
     up, down, left, right, switch, state = trackball.read()
 
     # Send movements and clicks to xte
-    if use_xte:
-        if switch:
-            cmd = 'xte "mouseclick 1"'
-            os.system(cmd)
-        elif right or up or left or down:
-            x = right - left
-            x = math.copysign(x**2, x)
-            y = down - up
-            y = math.copysign(y**2, y)
-            cmd = 'xte "mousermove {} {}"'.format(x, y)
-            os.system(cmd)
-    else:
-        print('xte not found')
+    if switch:
+        cmd = 'xte "mouseclick 1"'
+        os.system(cmd)
+    elif right or up or left or down:
+        x = right - left
+        x = math.copysign(x**2, x)
+        y = down - up
+        y = math.copysign(y**2, y)
+        cmd = 'xte "mousermove {} {}"'.format(int(x), int(y))
+        os.system(cmd)
 
     time.sleep(0.0001)
